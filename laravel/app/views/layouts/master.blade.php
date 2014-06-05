3544b5b460f92a604f155c0d555f805d0a26b33f
carlos.juarez <t7marketing004@gmail.com>
2014-06-05 11:52:51 -0500

    cambio de titulo

--- laravel/app/views/layouts/master.blade.php
+++ laravel/app/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
             <title>
                   @section('title')
-                  Primer Server
+                  T7Marketing
                   @show        
             </title>
             <meta name="description" content="">
